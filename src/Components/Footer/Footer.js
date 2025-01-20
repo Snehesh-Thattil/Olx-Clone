99e825187c967110c012df5203b2b40ef9173eb1
Snehesh Thattil <sneheshthattil75@gmail.com>
2025-01-20 14:12:09 +0530

    [ Footer modified with better design and providing links ]

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,53 +4,66 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="footerParentDiv">
-      <div className="content">
-        <div>
-          <div className="heading">
-            <p>POPULAR LOCATIONS</p>
-          </div>
-          <div className="list">
-            <ul>
-              <li>kolkata</li>
-              <li>Mumbai</li>
-              <li>Chennai</li>
-              <li>Pune</li>
-            </ul>
-          </div>
+    <div className="Footer">
+      <div className="links">
+        <div className="popular-loc">
+          <h3>Popular Locations</h3>
+          <a href="https://www.olx.in/en-in/kolkata_g4157275">Kolkata</a>
+          <a href="https://www.olx.in/en-in/mumbai_g4058997">Mumbai</a>
+          <a href="https://www.olx.in/en-in/chennai_g4059162">Chennai</a>
+          <a href="https://www.olx.in/en-in/pune_g4059014">Pune</a>
         </div>
-        <div>
-          <div className="heading">
-            <p>ABOUT US</p>
-          </div>
-          <div className="list">
-            <ul>
-              <li>About OLX Group</li>
-              <li>Careers</li>
-              <li>Contact Us</li>
-              <li>OLXPeople</li>
-            </ul>
-          </div>
+        <div className="trending-loc">
+          <h3>Trending Locations</h3>
+          <a href="https://www.olx.in/en-in/bhubaneshwar_g4059049">Bhubaneshwar</a>
+          <a href="https://www.olx.in/en-in/hyderabad_g4058526">Hyderabad</a>
+          <a href="https://www.olx.in/en-in/chandigarh_g4058651">Chandigarh</a>
+          <a href="https://www.olx.in/en-in/nashik_g4059002">Nashik</a>
         </div>
-        <div>
-          <div className="heading">
-            <p>OLX</p>
+        <div className="about">
+          <h3>ABOUT US</h3>
+          <a href="https://tech.olx.in/">Tech@OLX</a>
+        </div>
+        <div className="olx-links">
+          <h3>OLX</h3>
+          <a href="https://www.olx.in/blog">blog</a>
+          <a href="https://help.olx.in/hc/en-us">Help</a>
+          <a href="https://www.olx.in/en-in/sitemap/most-popular">Sitemap</a>
+          <a href="https://help.olx.in/hc/en-us/categories/10781706981149-Legal-Privacy-information">Legal & Privacy information</a>
+          <a href="https://bugbase.ai/programs/olx">Vulnarability Disclosure Program</a>
+        </div>
+        <div className="follow-us">
+          <h3>FOLLOW US</h3>
+          <div className="icons">
+            <a href="https://www.facebook.com/olxindia/"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://www.instagram.com/olx_india/"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://twitter.com/OLX_India"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.youtube.com/user/OLXInTv"><i class="fa-solid fa-circle-play"></i></a>
           </div>
-          <div className="list">
-            <ul>
-              <li>Help</li>
-              <li>Sitemap</li>
-              <li>Legal & Privacy information</li>
-            </ul>
+          <div className="store">
+            <a href="https://play.google.com/store/apps/details?id=com.olx.southasia"><img src="https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png" alt="" /></a>
+            <a href="https://itunes.apple.com/in/app/olx-buy-sell-near-you/id913492792?mt=8"><img src="https://b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png" alt="" /></a>
           </div>
         </div>
       </div>
-      <div className="footer">
-        <p>Other Countries Pakistan - South Africa - Indonesia</p>
-        <p>Free Classifieds in India. © 2006-2021 OLX</p>
+      <div className="subsidiaries">
+        <div className="logos">
+          <a href="/"><img src="https://statics.olx.in/external/base/img/cartrade/logo/cartrade_tech.svg?v=1" alt="" /></a>
+          <div className="hr-line"></div>
+          <a href="/"><img src="https://statics.olx.in/external/base/img/cartrade/logo/olx.svg?v=1" alt="" /></a>
+          <a href="https://www.carwale.com/"><img src="https://statics.olx.in/external/base/img/cartrade/logo/carwale.svg?v=1" alt="" /></a>
+          <a href="https://www.bikewale.com/"><img src="https://statics.olx.in/external/base/img/cartrade/logo/bikewale.svg?v=1" alt="" /></a>
+          <a href="https://www.cartrade.com/"><img src="https://statics.olx.in/external/base/img/cartrade/logo/cartrade.svg?v=1" alt="" /></a>
+          <a href="https://www.mobilityoutlook.com/"><img src="https://statics.olx.in/external/base/img/cartrade/logo/mobility.svg?v=1" alt="" /></a>
+        </div>
+
+        <div className="help">
+          <a href="https://www.olx.in/en-in/sitemap/most-popular">Help - Sitemap</a>
+          <p>All rights reserved © 2006-2025 OLX</p>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default Footer;
